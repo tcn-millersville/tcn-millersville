47feaf2ab1897b441877d7af4d027aa920bb0bdf
Justin <jmms5757@gmail.com>
2024-01-08 23:55:49 -0500

    update prod url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/tcn-millersville" : "/",
 };
 
 
