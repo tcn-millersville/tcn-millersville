daa8939f47f6d8092533872c0e4d8dbad54a9add
Justin <jmms5757@gmail.com>
2024-03-02 19:57:44 -0500

    cleaned up css styles

--- src/components/ClubMeeting.vue
+++ src/components/ClubMeeting.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div>
-    <h1 style="text-align: center; font-size: 200%"><strong>{{ meetingTitle }}</strong></h1>
+    <h1><strong>{{ meetingTitle }}</strong></h1>
     <div class="video-container">
       <iframe width="600" height="400" :src="videoSource" allowfullscreen></iframe>
     </div>
@@ -24,54 +24,14 @@
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-.img-wrapper img {
-  width: 60%;
-}
-
-.link-arrow p,
-.link-arrow2 p {
-  display: inline-block;
-  transition: 0.1s ease-in;
-}
-
-.link-arrow:hover p,
-.link-arrow2:hover p {
-  transform: translateX(50%);
-}
-
 p {
   font-style: normal;
   font-weight: 500;
   font-size: 18px;
 }
 
-.text-box h2 {
-  border-bottom: 2px solid #EEB111;
-}
-
-.col h2 {
-  padding-top: 30px;
-  font-weight: 535;
-}
-
-.icon-banner p {
+h1 {
   text-align: center;
-  padding-bottom: 0;
-  padding-right: 20px;
-  padding-left: 20px;
+  font-size: 200%;
 }
 </style>
